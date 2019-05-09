05fa4cd303d24af5ad97cc39ec00b7ab47753108
Raulino Neto <raulinoneto91@gmail.com>
2019-05-09 09:58:40 -0300

    configuration and routes defined

---
+++ config/config.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => getenv('APP_DEBUG') === 'true' ? true : false, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        // App Settings
+        'app' => [
+            'name' => getenv('APP_NAME'),
+            'url'  => getenv('APP_URL'),
+            'env'  => getenv('APP_ENV'),
+        ],
+        // Monolog settings
+        'logger' => [
+            'name'  => getenv('APP_NAME'),
+            'path'  => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+        // Database settings
+        'database' => [
+            'driver'    => getenv('DB_CONNECTION'),
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_DATABASE'),
+            'username'  => getenv('DB_USERNAME'),
+            'password'  => getenv('DB_PASSWORD'),
+            'port'      => getenv('DB_PORT'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+        'cors' => null !== getenv('CORS_ALLOWED_ORIGINS') ? getenv('CORS_ALLOWED_ORIGINS') : '*'
+    ],
+];
---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+use App\Controllers\BreedsController;
+
+// Api Routes
+$app->group('/',
+    function () {
+        // Breeds Routes
+        $this->get('/breeds', BreedsController::class . ':search');
+        $this->get('/breeds/{ID}', BreedsController::class . ':show');
+        
+});
